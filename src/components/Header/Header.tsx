e0ada7134e28c2382e6427f64ba9de011ebcada7
Heorhi Zaturkevich <konisgung@gmail.com>
2024-01-19 22:19:56 +0300

    init

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { GlobalImage } from '../../assets/images/GlobalImage';
+import s from './Header.module.scss';
+
+export const Header = () => {
+  return (
+    <header className={s.header}>
+      <GlobalImage id="plane" />
+      <h1 className={s.header__title}>Поиск авиабилетов</h1>
+    </header>
+  );
+};
